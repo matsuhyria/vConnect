f938fa65950b5d9c10cb1bd4dc235e97391fc51a
Mahmoud Khalafallah <mahmoudk@student.chalmers.se>
2024-09-15 23:53:09 +0200

    fix user deletion

--- server/contollers/userController.js
+++ server/contollers/userController.js
@@ -59,7 +59,7 @@ const updateUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {
     try {
-        const user = await User.findByIdAndRemove(req.params.id);
+        const user = await User.findByIdAndDelete(req.params.id);
         if (!user) return res.status(404).json({ message: "User not found" });
         res.json({ message: "User deleted!", user });
     } catch (err) {
