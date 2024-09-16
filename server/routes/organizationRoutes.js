1de162b2db61609c4ba6f72219aea6c4bbb13bc9
Maksym <maksymm@student.chalmers.se>
2024-09-16 22:18:31 +0200

    add organizationRouter

--- server/routes/organizationRoutes.js
+++ server/routes/organizationRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { createOrganization } = require('../contollers/organizationController');
+const { createOrganization, getAllOrganizations, getOrganizationById, updateOrganizationById, deleteOrganizationById} = require('../contollers/organizationController');
+
+console.log('Function is defined', deleteOrganizationById);
 
 router.post('/api/v1/organizations', createOrganization);
+router.get('/api/v1/organizations', getAllOrganizations);
+router.get('/api/v1/organizations/:id', getOrganizationById);
+router.patch('/api/v1/organizations/:id', updateOrganizationById);
+router.put('/api/v1/organizations/:id', updateOrganizationById);
+router.delete('/api/v1/organizations/:id', deleteOrganizationById);
 
 module.exports = router;
