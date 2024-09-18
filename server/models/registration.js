92cb627abaf7bcbfde8a7e8dc560645573b509be
Maksym <maksymm@student.chalmers.se>
2024-09-18 11:53:04 +0200

    add registration schema

---
+++ server/models/registration.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const registration = new Schema({
+    user: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true
+    },
+    opportunity: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'Opportunity', 
+        required: true 
+    },
+    registrationDate: { 
+        type: Date, 
+        default: Date.now 
+    },
+    status: { 
+        type: String, 
+        enum: ['pending', 'confirmed', 'canceled'], 
+        default: 'pending' 
+    }
+});
