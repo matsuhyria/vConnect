3c3503f4798fc45ec5232f93c6abcba879f7e719
Mahmoud Khalafallah <mahmoudk@student.chalmers.se>
2024-09-19 00:11:02 +0200

    add opportunityRoutes middleware

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var cors = require('cors');
 var history = require('connect-history-api-fallback');
 var userRoutes = require('./routes/userRoutes');
 var orgRoutes = require('./routes/organizationRoutes');
+var opportunityRoutes = require('./routes/opportunityRoutes');
 
 // Variables
 var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/volunteerconnect_db';
@@ -37,8 +38,8 @@ app.get('/api', function (req, res) {
 });
 
 app.use(userRoutes);
-
 app.use(orgRoutes);
+app.use(opportunityRoutes);
 
 // Catch all non-error handler for api (i.e., 404 Not Found)
 app.use('/api/*', function (req, res) {
