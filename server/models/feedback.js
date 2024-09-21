974c41666d68076132a8f7a831cfb49c94723eaf
Mahmoud Khalafallah <mahmoudk@student.chalmers.se>
2024-09-21 14:16:05 +0200

    #7 indexing fields

--- server/models/feedback.js
+++ server/models/feedback.js
@@ -1,32 +1,34 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema; 
+const Schema = mongoose.Schema;
 
 const feedback = new Schema({
-    user: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'User', 
-        required: true 
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-    opportunity: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'Opportunity', 
-        required: true 
+    opportunity: {
+        type: Schema.Types.ObjectId,
+        ref: 'Opportunity',
+        required: true
     },
-    registrationDate: { 
-        type: Date, 
-        default: Date.now 
+    date: {
+        type: Date,
+        default: Date.now
     },
-    rating: { 
-        type: Number, 
-        required: true, 
-        min: 1, 
-        max: 5 
-    },  
-    comment: { 
-        type: String, 
-        maxlength: 500 
-    } 
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    comment: {
+        type: String,
+        maxlength: 500
+    }
 });
+
+feedback.index({ user: 1, opportunity: 1 }, { unique: true });
 
 const Feedback = mongoose.model('Feedback', feedback);
 module.exports = Feedback;
--- server/models/registration.js
+++ server/models/registration.js
@@ -2,25 +2,28 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const registration = new Schema({
-    user: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'User', 
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
         required: true
     },
-    opportunity: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'Opportunity', 
-        required: true 
+    opportunity: {
+        type: Schema.Types.ObjectId,
+        ref: 'Opportunity',
+        required: true
     },
-    registrationDate: { 
-        type: Date, 
-        default: Date.now 
+    date: {
+        type: Date,
+        default: Date.now
     },
-    status: { 
-        type: String, 
-        enum: ['pending', 'confirmed', 'canceled'], 
-        default: 'pending' 
+    status: {
+        type: String,
+        enum: ['pending', 'confirmed', 'no_show', 'canceled'],
+        default: 'pending'
     }
 });
+
+registration.index({ user: 1, opportunity: 1 }, { unique: true }); // Ensures unique registrations for each user-opportunity pair
+registration.index({ status: 1 });
 
 module.exports = mongoose.model('Registration', registration);
