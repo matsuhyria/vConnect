bf6f7332299f5087ec5c723776496b96ad532f53
Mahmoud Khalafallah <mahmoudk@student.chalmers.se>
2024-09-29 15:09:56 +0200

    group contants

---
+++ server/helpers/constants.js
@@ -0,0 +1,15 @@
+const BASE_PATH = '/api/v1';
+const MONGODB_URI = process.env.MONGODB_URI;
+const TOKEN_SECRET = process.env.TOKEN_SECRET || '<PASSWORD>';
+const PORT = process.env.PORT || '3000';
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const TOKEN_COOKIE_NAME = 'token';
+
+module.exports = {
+    BASE_PATH,
+    MONGODB_URI,
+    TOKEN_SECRET,
+    PORT,
+    NODE_ENV,
+    TOKEN_COOKIE_NAME
+};
