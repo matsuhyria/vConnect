cd8f1f4d24efeb5120be2cd1a5578ab3f95abded
Mahmoud Khalafallah <mahmoudk@student.chalmers.se>
2024-09-29 15:12:41 +0200

    #11 update routes

--- server/routes/opportunityRoutes.js
+++ server/routes/opportunityRoutes.js
@@ -1,19 +1,56 @@
 const express = require("express");
 const router = express.Router();
 
-const { createOpportunity, getOpportunities, getOpportunity, getOpportunitiesByOrg, updateOpportunity, deleteOpportunity } = require("../controllers/opportunityController");
+const { BASE_PATH } = require("../helpers/constants");
+const {
+    createOpportunity,
+    getOpportunities,
+    getOpportunity,
+    getOpportunitiesPerOrganization,
+    updateOpportunity,
+    deleteOpportunity
+} = require("../controllers/opportunityController");
+const verifyAccess = require("../middlewares/auth/verifyAccess");
+const verifyOrganizationManager = require("../middlewares/auth/verifyOrganizationManager");
 
-router.route('/api/v1/opportunities')
-    .post(createOpportunity)
-    .get(getOpportunities);
+// Define routes for opportunities API.
+router.route(`${BASE_PATH}/opportunities`)
+    // Get all opportunities
+    .get(getOpportunities)
+    // Create a new opportunity
+    .post(
+        verifyAccess({ requiredType: 'organization_representative' }),
+        verifyOrganizationManager(),
+        createOpportunity
+    );
 
-router.route('/api/v1/opportunities/:id')
+// Define route parameters and HTTP methods for individual opportunities.
+router.route(`${BASE_PATH}/opportunities/:id`)
+    // Get an existing opportunity by ID
     .get(getOpportunity)
-    .put(updateOpportunity)
-    .patch(updateOpportunity)
-    .delete(deleteOpportunity);
+    // Update an existing opportunity
+    .put(
+        verifyAccess({ requiredType: 'organization_representative' }),
+        verifyOrganizationManager(),
+        updateOpportunity
+    )
+    // Patch an existing opportunity
+    .patch(
+        verifyAccess({ requiredType: 'organization_representative' }),
+        verifyOrganizationManager(),
+        updateOpportunity
+    )
+    // Delete an existing opportunity
+    .delete(
+        verifyAccess({ requiredType: 'organization_representative' }),
+        verifyOrganizationManager(),
+        deleteOpportunity
+    );
 
-router.route('/api/v1/organizations/:id/opportunities')
-    .get(getOpportunitiesByOrg);
+// Define route for getting opportunities by organization.
+router.route(`${BASE_PATH}/organizations/:id/opportunities`)
+    // Get all opportunities for a specific organization
+    .get(getOpportunitiesPerOrganization);
+
 
 module.exports = router;
--- server/routes/registrationRoutes.js
+++ server/routes/registrationRoutes.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
+const { BASE_PATH } = require('../helpers/constants');
 const {
     createRegistration,
-    getAllRegistrations,
+    getAllUserRegistrations,
     getRegistrationById,
-    getRegistrationByOppId,
+    getRegistrationsPerOpportunity,
     updateRegistrationById,
     deleteRegistrationById
 } = require('../controllers/registrationController');
+const verifyAccess = require('../middlewares/auth/verifyAccess');
+const verifyRegistrationOwnership = require('../middlewares/auth/verifyRegistrationOwnership');
 
-router.route('/api/v1/users/:user_id/opportunities/:opportunity_id/registrations')
-    .post(createRegistration)
-    .get(getAllRegistrations);
 
-router.route('/api/v1/users/:user_id/opportunities/:opportunity_id/registrations/:id')
-    .patch(updateRegistrationById)
+// Define routes for individual registrations
+router.route(`${BASE_PATH}/registrations/:id`)
+    // Retrieve a specific registration by ID
+    .get(verifyAccess(), verifyRegistrationOwnership(), getRegistrationById)
+    // Update an existing registration with the provided data (PUT request)
+    .put(verifyAccess(), verifyRegistrationOwnership(), updateRegistrationById)
+    // Partially update an existing registration with the provided data (PATCH request)
+    .patch(verifyAccess(), verifyRegistrationOwnership(), updateRegistrationById)
+    // Delete a specific registration by ID
+    .delete(verifyAccess(), verifyRegistrationOwnership(), deleteRegistrationById);
 
-router.route('/api/v1/registrations/:id')
-    .get(getRegistrationById)
-    .put(updateRegistrationById)
-    .delete(deleteRegistrationById);
+// Routes for User Registrations
+router.route(`${BASE_PATH}/registrations`)
+    // Retrieve all registrations for a specific user
+    .get(verifyAccess(), getAllUserRegistrations);
 
-router.route('/api/v1/opportunities/:id/registrations')
-    .get(getRegistrationByOppId);
+
+// Define routes for opportunities and registrations
+router.route(`${BASE_PATH}/opportunities/:id/registrations`)
+    // Create a new registration when the endpoint is hit with a POST request
+    .post(verifyAccess(), createRegistration)
+    // Retrieve all registrations for a given opportunity
+    .get(verifyAccess(), getRegistrationsPerOpportunity);
 
 module.exports = router;
