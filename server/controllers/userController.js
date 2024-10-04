0c68dc7a0ffd5bb601514aa256617ed063505e5b
Mahmoud Khalafallah <mahmoudk@student.chalmers.se>
2024-10-05 00:21:50 +0200

    add admin user endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,15 +3,51 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+const addUser = async (password, name, email, type) => {
+    const encryptedPassword = await bcrypt.hash(password.toString(), saltRounds);
+    const user = new User({
+        name, email, password: encryptedPassword, type
+    });
+
+    await user.save();
+    return user;
+};
+
 const createUser = async (req, res) => {
     try {
         const { name, email, password, type } = req.body;
-        const encryptedPassword = await bcrypt.hash(password.toString(), saltRounds);
-        const user = new User({
-            name, email, password: encryptedPassword, type
-        });
 
-        await user.save();
+        if (type === 'admin') {
+            res.status(400).json({ message: 'Invalid user type' });
+        }
+
+        const user = await addUser(password, name, email, type);
+
+        // Generate JWT token
+        const token = generateToken({ userId: user._id, email, type: user.type });
+
+        res.status(201).json({ token, user: { id: user._id, email: user.email, type: user.type } });
+    } catch (err) {
+        console.error(err);
+        return res.status(400).send({ message: err.message });
+    }
+};
+
+const createAdminUser = async (req, res) => {
+    try {
+        const { name, email, password, type } = req.body;
+
+        const adminExists = await User.findOne({ type: 'admin' });
+
+        if (adminExists) {
+            return res.status(409).json({ message: 'Admin exists!' });
+        }
+
+        if (type !== 'admin') {
+            return res.status(400).json({ message: 'Invalid user type' });
+        }
+
+        const user = await addUser(password, name, email, type);
 
         // Generate JWT token
         const token = generateToken({ userId: user._id, email, type: user.type });
@@ -24,8 +60,9 @@ const createUser = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-    const { email, password } = req.body;
     try {
+        const { email, password } = req.body;
+
         const user = await User.findOne({ email });
         if (!user) return res.status(404).json({ message: 'User not found' });
 
@@ -56,12 +93,18 @@ const getUser = async (req, res) => {
 const updateUser = async (req, res) => {
     try {
         const { name, email, password, type } = req.body;
+
+        if (type === 'admin') {
+            res.status(400).json({ message: 'Invalid user type' });
+        }
+
         const encryptedPassword = await bcrypt.hash(password.toString(), saltRounds);
         const user = await User.findByIdAndUpdate(
             req.params.id,
             { $set: { name, email, password: encryptedPassword, type } },
             { new: true }
         );
+
         if (!user) return res.status(404).json({ message: 'User not found' });
 
         // Generate JWT token
@@ -85,4 +128,4 @@ const deleteUser = async (req, res) => {
     }
 };
 
-module.exports = { createUser, loginUser, getUser, updateUser, deleteUser };
+module.exports = { createUser, loginUser, getUser, updateUser, deleteUser, createAdminUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { BASE_PATH } = require('../helpers/constants');
-const { createUser, getUser, updateUser, deleteUser, loginUser } = require('../controllers/userController');
+const { createUser, createAdminUser, getUser, updateUser, deleteUser, loginUser } = require('../controllers/userController');
 const verifyAccess = require('../middlewares/auth/verifyAccess');
+// addUser('123', 'admin', 'admin@vconnect.com', 'admin');
 
 // Define routes for creating a new user and logging in
 router.post(`${BASE_PATH}/users`, createUser);
+router.post(`${BASE_PATH}/users/admin`, createAdminUser);
 router.post(`${BASE_PATH}/users/login`, loginUser);
 
 // Define routes for retrieving, updating, and deleting a specific user
