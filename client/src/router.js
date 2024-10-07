35c6998662b43b3cad714837ebdfc6939df4835d
Maksym <maksymm@student.chalmers.se>
2024-10-08 00:21:40 +0200

    #16 add initial layout

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home }
+  { path: '/', name: 'home', component: Home },
+  { path: '/login', name: 'login', component: Login }
 ]
 
 const router = createRouter({
---
+++ client/src/views/Login.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div>
+    <div class="logo mt-5">
+      <img src="/logo-b.svg" alt="Logo" width="150" />
+    </div>
+    <div class="d-flex justify-content-center align-items-center">
+      <div class="border rounded p-5 ">
+        <h2 class="fw-bold mb-5">Login</h2>
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <button type="submit" class="btn btn-dark">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
