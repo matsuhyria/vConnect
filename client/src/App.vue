84f3efc8a8a031d5c0c9234fb735d3198db640e1
Maksym <maksymm@student.chalmers.se>
2024-10-07 21:25:22 +0200

    #20 add form field component

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,10 +3,24 @@
     <div id="nav">
       <router-link to="/">Home</router-link>
     </div>
+    <Navbar />
     <!-- Render the content of the current page view -->
-    <router-view/>
+    <router-view />
+    <InputField label="Email" inputType="email" v-model="model" placeholder="Enter email" required="true"/>
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import InputField from './components/InputField.vue'
+
+export default {
+  components: {
+    Navbar,
+    InputField,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ client/src/components/InputField.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mb-3">
+    <label for="id" class="form-label"> {{ label }} </label>
+    <input id="id" :type="inputType" :v-model="value" :placeholder="placeholder" :required="required" class="form-control" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: 'Input'
+    },
+    inputType: {
+      type: String,
+      default: 'text'
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    required: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+
+</script>
+
+<style scoped></style>
