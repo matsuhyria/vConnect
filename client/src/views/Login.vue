7156bcef84111d91dd76def158ca4c836170248c
Maksym <maksymm@student.chalmers.se>
2024-10-10 21:52:25 +0200

    #16 use <input> instead of <inputField>

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import Api from '@/Api'
+import api from '@/Api'
 import InputField from '@/components/InputField.vue'
 
 const form = reactive({
@@ -18,7 +18,7 @@ const loginUser = async () => {
     return
   }
   try {
-    await Api.login({
+    await api.login({
       email: form.email,
       password: form.password
     })
@@ -40,18 +40,26 @@ const loginUser = async () => {
           {{ errorMessage }}
         </div>
         <form @submit.prevent="loginUser">
-          <inputField
-            label="Email Address"
-            inputType="email"
-            v-model="form.email"
-            required
-          />
-          <inputField
-            label="Password"
-            inputType="password"
-            v-model="form.password"
-            required
-          />
+          <div class="mb-3 text-start">
+            <label for="email" class="form-label fw-semibold">Email address</label>
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              v-model="form.email"
+              required
+            />
+          </div>
+          <div class="mb-3 text-start">
+            <label for="password" class="form-label fw-semibold">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              v-model="form.password"
+              required
+            />
+          </div>
           <button type="submit" class="btn btn-dark w-50 mt-3">Login</button>
         </form>
         <p class="mt-3 text-center">
