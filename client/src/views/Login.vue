c4535ad03e98badf5c9241456bab51bc1620ee54
Maksym <maksymm@student.chalmers.se>
2024-10-10 21:48:20 +0200

    #16 add login page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,29 +1,74 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import Api from '@/Api'
+import InputField from '@/components/InputField.vue'
+
+const form = reactive({
+  email: '',
+  password: ''
+})
+
+const errorMessage = ref(null)
+const router = useRouter()
+
+const loginUser = async () => {
+  if (!form.email || !form.password) {
+    errorMessage.value = 'Email and password are required'
+    return
+  }
+  try {
+    await Api.login({
+      email: form.email,
+      password: form.password
+    })
+    // Redirect to the home page afterwards
+    router.push('/')
+  } catch (error) {
+    errorMessage.value = 'Login failed. Please check your credentials'
+  }
+}
 </script>
 
 <template>
   <div>
-    <div class="logo mt-5">
-      <img src="/logo-b.svg" alt="Logo" width="150" />
-    </div>
-    <div class="d-flex justify-content-center align-items-center">
-      <div class="border rounded p-5 ">
-        <h2 class="fw-bold mb-5">Login</h2>
-        <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" />
-          </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-          </div>
-          <button type="submit" class="btn btn-dark">Login</button>
+    <img :src="`./logo-b.svg`" alt="logo" width="190" class="my-4" />
+    <div class="wrapper m-auto border rounded">
+      <div class="form-container mt-5">
+        <h2 class="mb-4 text-center">Login</h2>
+        <div v-if="errorMessage" class="alert alert-danger">
+          {{ errorMessage }}
+        </div>
+        <form @submit.prevent="loginUser">
+          <inputField
+            label="Email Address"
+            inputType="email"
+            v-model="form.email"
+            required
+          />
+          <inputField
+            label="Password"
+            inputType="password"
+            v-model="form.password"
+            required
+          />
+          <button type="submit" class="btn btn-dark w-50 mt-3">Login</button>
         </form>
+        <p class="mt-3 text-center">
+          Don't have an account? <router-link to="/register">Register</router-link>
+        </p>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.form-container {
+  max-width: 80%;
+  margin: auto;
+}
+
+.wrapper {
+  max-width: 460px;
+}
+</style>
