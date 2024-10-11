8e174b26d5cf3c410fe4c96bd6b1a306be6e9b5a
Mahmoud Khalafallah <mahmoudk@student.chalmers.se>
2024-10-12 00:15:05 +0200

    #27 add new icons

---
+++ client/src/components/icons/BuildingIcon.vue
@@ -0,0 +1,58 @@
+<template>
+  <svg
+    width="13"
+    height="16"
+    viewBox="0 0 13 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M4.62695 5.78145L4.63592 5.77264"
+      stroke="#7D7D7D"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8.25342 5.78145L8.26238 5.77264"
+      stroke="#7D7D7D"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M4.62695 8.96376L4.63592 8.95496"
+      stroke="#7D7D7D"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8.25342 8.96376L8.26238 8.95496"
+      stroke="#7D7D7D"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M4.62695 12.1461L4.63592 12.1373"
+      stroke="#7D7D7D"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8.25342 12.1461L8.26238 12.1373"
+      stroke="#7D7D7D"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M1 14.8432V3.06852C1 2.80489 1.24357 2.59117 1.54402 2.59117H6.44017V1.47735C6.44017 1.21372 6.68371 1 6.98419 1H11.3363C11.6368 1 11.8803 1.21372 11.8803 1.47735V14.8432C11.8803 15.1068 11.6368 15.3205 11.3363 15.3205H1.54402C1.24357 15.3205 1 15.1068 1 14.8432Z"
+      stroke="#7D7D7D"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
---
+++ client/src/components/icons/EmailIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    width="15"
+    height="11"
+    viewBox="0 0 15 11"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M14 2.16077L7.5 6.22327L1 2.16077"
+      stroke="#7D7D7D"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M2.3 1H12.7C13.415 1 14 1.52232 14 2.16071V9.125C14 9.76339 13.415 10.2857 12.7 10.2857H2.3C1.585 10.2857 1 9.76339 1 9.125V2.16071C1 1.52232 1.585 1 2.3 1Z"
+      stroke="#7D7D7D"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
