093ef042bf6d64915a1d008d888ea77098ae285f
Mahmoud Khalafallah <mahmoudk@student.chalmers.se>
2024-10-12 00:25:44 +0200

    #27 utilise OpportunityCard component in Opportunities view

--- client/src/views/Opportunities.vue
+++ client/src/views/Opportunities.vue
@@ -1,20 +1,15 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import api from '@/Api'
-import MapIcon from '@/components/icons/MapIcon.vue'
+import OpportunityCard from '@/components/OpportunityCard.vue'
 
 const opportunities = ref([])
 const loading = ref(true)
 const errorMessage = ref(null)
 
-const truncateDate = (date) => {
-  const res = new Date(date)
-  return res.toLocaleDateString('en-CA')
-}
-
 const matchOrganizationName = async (orgId) => {
   try {
-    const organization = await api.getOrganizationById(orgId)
+    const organization = await api.getOrganization(orgId)
     return organization.name
   } catch (error) {
     console.error('Error fetching organization name:', error)
@@ -25,10 +20,12 @@ const matchOrganizationName = async (orgId) => {
 const addOrgNameToOpportunities = async (opportunities) => {
   return await Promise.all(
     opportunities.map(async (opportunity) => {
-      const organizationName = await matchOrganizationName(opportunity.organizationId)
+      const organizationName = await matchOrganizationName(
+        opportunity.organizationId
+      )
       return {
         ...opportunity, // spread the properties of an opportunity into a new object
-        organizationName: organizationName || 'Unknown Organization' // add organizationName field to a new object
+        organizationName // add organizationName field to a new object
       }
     })
   )
@@ -53,11 +50,14 @@ onMounted(() => {
 
 <template>
   <div class="container my-5">
-    <div class="d-flex flex-column flex-md-row justify-content-between align-items-start mb-4">
+    <div
+      class="d-flex flex-column flex-md-row justify-content-between align-items-start mb-4"
+    >
       <div class="text-start">
         <h1 class="h2 fw-bold">Volunteer Opportunities</h1>
         <p class="text-body-secondary">
-          Find the perfect volunteering opportunity that matches your interests and availability
+          Find the perfect volunteering opportunity that matches your interests
+          and availability
         </p>
       </div>
     </div>
@@ -74,28 +74,20 @@ onMounted(() => {
       <div v-if="!opportunities.length" class="alert">
         Ops!! No Opportunities to display
       </div>
-      <div v-for="opportunity in opportunities" :key="opportunity.id" class="col-sm-6 col-lg-4 col-xxl-3 mb-4">
-        <div class="card h-100">
-          <div class="card-body d-flex flex-column">
-            <h5 class="card-name fs-4 fw-semibold">{{ opportunity.title }}</h5>
-            <p class="card-text text-secondary">{{ opportunity.description }}</p>
-            <p class="text-body-secondary mt-auto">
-              <MapIcon /> {{ truncateDate(opportunity.date) }}
-            </p>
-            <p class="text-body-secondary">
-              <MapIcon /> {{ opportunity.address }}
-            </p>
-            <p class="text-body-secondary">
-              <MapIcon /> {{ opportunity.organizationName }}
-            </p>
-            <a href="#" class="btn border-secondary-subtle w-100">Read more</a>
-          </div>
-        </div>
+      <div
+        v-for="opportunity in opportunities"
+        :key="opportunity.id"
+        class="col-sm-6 col-lg-4 col-xxl-3 mb-4"
+      >
+        <OpportunityCard
+          :title="opportunity.title"
+          :description="opportunity.description"
+          :id="opportunity._id"
+          :address="opportunity.address"
+          :date="opportunity.date"
+          :organization="opportunity.organizationName"
+        />
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
