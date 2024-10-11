f924170699a1b32173256d3f77fe4a00472c5330
Maksym <maksymm@student.chalmers.se>
2024-10-11 03:06:10 +0200

    #23 add homepage

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,29 @@
 <script setup>
-// @ is an alias to /src
-import Api from '@/Api'
+import api from '@/Api'
+import Sidebar from '@/components/Sidebar.vue'
+
 </script>
 
 <template>
-  <div>
-    <b-container fluid>
-      <h1 class="display-5 fw-bold">VConnect</h1>
-    </b-container>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-lg-9">
+        <h1 class="fw-bold">Discover Volunteer Opportunities</h1>
+        <p>Browse our featured volunteer events and sign up to make a difference in your community!</p>
+        <div class="btn-container">
+          <router-link to="/opportunities" class="btn btn-dark me-3">Explore Opportunities</router-link>
+          <router-link to="/organizations" class="btn btn-light border">View Organizations</router-link>
+        </div>
+      </div>
+      <div class="col-12 col-lg-3 mt-4">
+        <Sidebar />
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.container {
+  margin-top: 10%;
+}
+</style>
