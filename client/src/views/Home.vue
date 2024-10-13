e3b6f1eb7c18967664d72e68ff2ef1683e12f5aa
Mahmoud Khalafallah <mahmoudk@student.chalmers.se>
2024-10-13 16:04:05 +0200

    update homepage styling

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,32 +3,27 @@ import Sidebar from '@/components/Sidebar.vue'
 </script>
 
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-12 col-lg-9">
+  <div class="container mt-5 pt-lg-5">
+    <div class="row justify-content-between">
+      <div class="col-xxl-5 col-md-6 mb-5 py-4">
         <h1 class="fw-bold">Discover Volunteer Opportunities</h1>
-        <p>
+        <p class="text-body-secondary my-4 fs-5">
           Browse our featured volunteer events and sign up to make a difference
           in your community!
         </p>
-        <div class="btn-container">
+        <div class="gap-2 d-flex flex-wrap">
           <router-link to="/opportunities" class="btn btn-dark me-3"
             >Explore Opportunities</router-link
           >
-          <router-link to="/organizations" class="btn btn-light border"
+          <router-link to="/organizations" class="btn btn-light border border-black"
             >View Organizations</router-link
           >
         </div>
       </div>
-      <div class="col-12 col-lg-3 mt-lg-0 mt-4">
-        <Sidebar />
-      </div>
+      <Sidebar class="col-lg-4 col-md-5" />
     </div>
   </div>
 </template>
 
 <style scoped>
-.container {
-  margin-top: 10%;
-}
 </style>
